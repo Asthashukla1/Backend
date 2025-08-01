aa1209a2274929a2bd6a39aa9f79b33eaefce02f
Astha Shukla <asthashukla160428@gmail.com>
2025-08-01 21:54:29 +0530

    mini todo app

--- 1Node_Demo/script.js
+++
@@ -1,7 +0,0 @@
-const { error } = require('console');
-const fs = require('fs');   //its a commonJS modeule can be converted into ES6
-const http = require('http')
-const server = http.createServer(function(req,res){
-    res.end("Hello server")
-})
-server.listen(3000);
